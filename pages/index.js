e962ed9d297a0f6a50abe2b13187ebf0726de7a0
Putindaldobro <putindaldobro@mail.ru>
2025-07-26 14:21:39 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      background: "black",
+      color: "gold",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "2rem"
+    }}>
+      ▽ Welcome to DALDOBRO
+    </div>
+  );
+}
